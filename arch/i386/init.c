1e512a6bbb09f1c5a579b16826fc04d54f02e45c
Alex Dzyoba <alex@dzyoba.com>
2016-01-14 17:38:22 +0300

    Add flat memory model GDT setup
    This commit introduce segmetn descriptor type, GDT and segments initialization
    routines to setup flat memory model with 4 segments:
    
    0. NULL segment (required)
    1. Kernel code segment 0-4 GiB, DPL=0
    2. Kernel data segment 0-4 GiB, DPL=0
    3. User code segment 0-4 GiB, DPL=3
    4. User data segment 0-4 GiB, DPL=3
    
    This setup is also suitable for future SYSENTER/SYSEXIT implementation.

--- arch/i386/init.c
+++ arch/i386/init.c
@@ -1,8 +1,12 @@
 #include <arch/init.h>
+#include <arch/i386/segment.h>
 #include <arch/i386/interrupts.h>
+
+#include <arch/i386/util.h>
 
 void arch_initialize(void)
 {
-	// gdt_init()
+	gdt_init();
+	__BREAK_BOCHS();
 	interrupts_init();
 }
